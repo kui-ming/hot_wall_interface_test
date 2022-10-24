6044a303b7f778e01ba2c9e780d16aec6ac2d655
shijingwen <15675611700@163.com>
2022-10-24 17:53:55 +0800

    配置MyBatisPlus的分页插件

---
+++ src/main/java/com/example/csust_hot_wall/configuration/MyBatisPlusConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.csust_hot_wall.configuration;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBatisPlusConfiguration {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 添加分页组件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+
+        return interceptor;
+    }
+
+}
