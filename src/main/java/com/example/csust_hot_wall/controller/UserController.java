73c7b8728f94626eacb9b83d4215e144efd95a96
shijingwen <15675611700@163.com>
2022-11-01 17:28:36 +0800

    查询openid

--- src/main/java/com/example/csust_hot_wall/controller/UserController.java
+++ src/main/java/com/example/csust_hot_wall/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.example.csust_hot_wall.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.example.csust_hot_wall.entity.User;
 import com.example.csust_hot_wall.service.UserService;
 import com.example.csust_hot_wall.tools.Message;
 import com.example.csust_hot_wall.tools.Utils;
+import com.example.csust_hot_wall.tools.weixin.OpenIdManage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,8 +20,18 @@ public class UserController extends BaseController<User,UserService>{
 
     @Autowired
     UserService userService;
+    @Autowired
+    OpenIdManage openIdManage;
 
     @PostMapping("/openid")
+    public Map openid(@RequestBody Map<String,String> data){
+        String code = data.get("code");
+        if (code == null) return Message.err(Message.Text.QUERY_ERR,"code不存在！");
+        JSONObject openId = openIdManage.getOpenId(code);
+        return Message.send(Message.Text.QUERY_SUCCESS,openId);
+    }
+
+    @GetMapping("infoByOpenid")
     public Map infoByOpenid(@RequestBody Map<String,String> data){
         String openid = data.get("openid");
         User user = userService.getByOpenid(openid);
